2bb5b5f3fe99222f5bc65b9d9007b58d4a7e3e71
Jason <jasonfouret@hotmail.com>
2022-10-28 17:47:31 +0200

    Create angular ptoject

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InitialWindowComponent } from './initial-window/initial-window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialWindowComponent
   ],
   imports: [
     BrowserModule
